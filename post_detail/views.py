be42019ad55e6d3d4a9941e564ae14f9e5149fe7
Enquil <jim.e.olesen@gmail.com>
2023-06-14 15:00:38 +0000

    added functionality to reply to other comments in post_detail.post statement

--- post_detail/views.py
+++ post_detail/views.py
@@ -47,3 +47,74 @@ class PostDetail(View):
                 'comment_form': CommentForm(),
             },
         )
+
+    def post(self, request, slug, *args, **kwargs):
+
+        # Only retrieves selected post if it has status: Published
+        post = get_object_or_404(
+
+            Post.objects.filter(status=1),
+            slug=slug
+
+        )
+
+        # Retrieves top-level comment parents
+        comments = post.comments.filter(
+
+            approved=True,
+            parent__isnull=True
+
+        ).order_by("-created_on")
+
+        comment_form = CommentForm(data=request.POST)
+
+        if comment_form.is_valid():
+
+            comment_form.instance.name = request.user.username
+            parent_obj = None
+            parent_id = None
+
+            # get parent comment id from hidden input (if it exists)
+            if request.POST.get('parent_id'):
+                # Make sures the id is an int
+                parent_id = int(request.POST.get('parent_id'))
+
+            '''
+            if parent_id exists in the form,
+            stores the comment from the comments queryset,
+            with corresponding id as parent_obj
+            '''
+            if parent_id:
+                parent_obj = comments.get(id=parent_id)
+                # if parent object exists
+                if parent_obj:
+                    # create reply comment object
+                    reply_comment = comment_form.save(commit=False)
+                    # assign parent_obj ID to reply comment as foreign key
+                    reply_comment.parent = parent_obj
+
+            # if NOT a reply comment
+            # create comment object
+            comment = comment_form.save(commit=False)
+            # assign comment to post
+            comment.post = post
+            # save
+            comment.save()
+            messages.success(
+                request, f'Your comment was succesfully posted'
+            )
+            return HttpResponseRedirect(reverse('post_detail', args=[slug]))
+
+        else:
+            comment_form = CommentForm()
+
+        return render(
+            request,
+            "post_detail/post_detail.html",
+            {
+                "post": post,
+                "comments": comments,
+                "comment_form": comment_form,
+                "liked": liked
+            },
+        )
