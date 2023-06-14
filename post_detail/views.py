4abbfcf9246c1f81aed6c64773227e966c51c165
Enquil <jim.e.olesen@gmail.com>
2023-06-14 12:23:25 +0000

    formatted code

--- post_detail/views.py
+++ post_detail/views.py
@@ -14,6 +14,14 @@ class PostDetail(View):
 
         queryset = Post.objects.filter(status=1)
         post = get_object_or_404(queryset, slug=slug)
+
+        comments = post.comments.filter(
+
+            approved=True,
+            parent__isnull=True
+
+        ).order_by("-created_on")
+
         template = 'post_detail/post_detail.html'
 
         return render(
