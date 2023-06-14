a19ebcfdc2360ddbfb511bf2f7fb62a4edfbf5d6
Enquil <jim.e.olesen@gmail.com>
2023-06-14 07:49:23 +0000

    created index.html and added newssite to installed apps

--- rtr_v3/settings.py
+++ rtr_v3/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
+from django.contrib.messages import constants as messages
+if os.path.isfile("env.py"):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +25,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qud5pzi7w&isal%yox8x3@teic8+!j9c5b6tu!0nljcy8h&9@g'
+SECRET_KEY = '+$jxfb@&3od^!!vzfn#^=(_ebbggx%m5jh_1+h3d5l3vx(ln6d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['8000-enquil-rtrv3-dthx7akst30.ws-eu99.gitpod.io']
 
 
 # Application definition
@@ -37,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'newssite',
 ]
 
 MIDDLEWARE = [
