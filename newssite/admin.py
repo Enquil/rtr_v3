dab9d28b9a728ff2e68405b08b7be50750339288
Enquil <jim.e.olesen@gmail.com>
2023-06-14 10:01:40 +0000

    made slug non-required in order to use custom save method, formatted code

--- newssite/admin.py
+++ newssite/admin.py
@@ -6,7 +6,6 @@ from django_summernote.admin import SummernoteModelAdmin
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
 
-    prepopulated_fields = {'slug': ('title',)}
     list_filter = ('status', 'created_on')
 
     list_display = (
--- newssite/models.py
+++ newssite/models.py
@@ -33,7 +33,7 @@ class Post(models.Model):
     '''
 
     title = models.CharField(max_length=200, unique=True)
-    slug = models.SlugField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True, blank=True)
 
     author = models.ForeignKey(
 
