d43573282eda472c2a21feae14b29d96c2a60598
Enquil <jim.e.olesen@gmail.com>
2023-06-15 15:47:09 +0000

    created context processor for categories to call them from base.html

--- rtr_v3/settings.py
+++ rtr_v3/settings.py
@@ -88,6 +88,9 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
 
+                # StackOverflow
+                'newssite.context_processors.categories',
+
             ],
         },
     },
