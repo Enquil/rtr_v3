7ed11502b8dea1e3e600242f5da2a8ae876d67c5
Enquil <jim.e.olesen@gmail.com>
2023-06-16 16:44:18 +0000

    created like functionality with post_like view and added like form to post_detail.html

--- post_detail/urls.py
+++ post_detail/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
-    # path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
+    path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
 ]
