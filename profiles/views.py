fa1e93b05692939588a6b1f6fd56fed6f1718373
Enquil <jim.e.olesen@gmail.com>
2023-06-25 10:21:59 +0000

    created basic profile view, connected urls and changed a href in base.html to link to profile

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from newssite.models import Post, Comment
+from django.views import View
+from django.shortcuts import (
+    render, get_object_or_404,
+    redirect, reverse
+)
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib import messages
+from .models import UserProfile
 
-# Create your views here.
+
+class ProfileView(View):
+
+    def get(self, request, *args, **kwargs):
+
+        profile = get_object_or_404(UserProfile, user=request.user)
+        print(profile.user)
+        return render(
+            request,
+            "profiles/profile.html",
+            {
+                'profile': profile,
+            },
+        )
