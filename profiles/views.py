df0163b3dbb8bd5e9ae512f2d7efe8bb6e4d7bf5
Enquil <jim.e.olesen@gmail.com>
2023-06-25 13:14:57 +0000

    added user posts and comments to profile template

--- profiles/views.py
+++ profiles/views.py
@@ -14,12 +14,18 @@ class ProfileView(View):
     def get(self, request, *args, **kwargs):
 
         if request.user.is_anonymous is False:
+
             profile = get_object_or_404(UserProfile, user=request.user)
+            posts = Post.objects.filter(author=profile.user.id)
+            comments = Comment.objects.filter(author=profile.user.id)
+
             return render(
                 request,
                 "profiles/profile.html",
                 {
                     'profile': profile,
+                    'posts': posts,
+                    'comments': comments,
                 },
             )
         else:
