e9cb86316025346cc95eddb902c446b80d441e6c
Enquil <jim.e.olesen@gmail.com>
2023-06-25 18:25:26 +0000

    added pagination handling and turned forms into links @ categories dropdown menu

--- newssite/views.py
+++ newssite/views.py
@@ -4,6 +4,7 @@ from django.views.generic import ListView
 from .models import Post
 from django.contrib import messages
 from django.core.paginator import Paginator, EmptyPage
+from django.utils.http import urlencode
 
 
 class PostList(ListView):
@@ -14,7 +15,8 @@ class PostList(ListView):
     ordering = ['-created_on']
     template_name = "newssite/index.html"
     # Sets number of items to be displayed per page
-    paginate_by = 3
+    paginate_by = 2
+    filtered = False
 
     def get_queryset(self):
         '''
@@ -22,9 +24,21 @@ class PostList(ListView):
         Lets user filter by category
         '''
         category = self.request.GET.get('category')
-
         if category is not None:
+            filtered = True
             return Post.objects.filter(
                 category=category).order_by('-created_on')
+        else:
+            return Post.objects.all().order_by('-created_on')
 
-        return Post.objects.all().order_by('-created_on')
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["category"] = self.request.GET.get('category')
+        print(context)
+        return context
+
+    def urlencode_filter(self):
+        qd = self.request.GET.copy()
+        print(qd)
+        qd.pop(self.page_kwarg, None)
+        return qd.urlencode()
