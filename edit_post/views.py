bc5d53c7f9f65e6dc9414c2147f91218d6121889
Enquil <jim.e.olesen@gmail.com>
2023-06-26 18:43:43 +0000

    created basic view for edit_post

--- edit_post/views.py
+++ edit_post/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from create_post.forms import PostForm
 
-# Create your views here.
+
+class EditPost(View):
+
+    def get(self, request, slug, *args, **kwargs):
+
+        template = 'post_detail/post_detail.html'
+
+        # Retrieves post based on slug, could be safer using id?
+        post = get_object_or_404(
+            Post.objects.filter(slug=slug),
+        )
