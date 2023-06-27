54008f4129368510c03ae9230e130f0c84342348
Enquil <jim.e.olesen@gmail.com>
2023-06-27 22:31:25 +0000

    added custom 404handler to urls

--- edit_post/urls.py
+++ edit_post/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 from django.contrib.auth.decorators import login_required
+handler404 = "edit_post.views.custom_404"
 
 urlpatterns = [
     path(
--- edit_post/views.py
+++ edit_post/views.py
@@ -9,27 +9,23 @@ from django.http import (HttpResponse,
                          HttpResponseRedirect)
 from create_post.forms import PostForm
 from django.views.generic import UpdateView
-from django.contrib.messages.views import SuccessMessageMixin
+from django.http import Http404
 
 
-class EditPost(SuccessMessageMixin, UpdateView):
+class EditPost(UpdateView):
 
     model = Post
     form_class = PostForm
     template_name = "edit_post/edit_post.html"
     success_url = "/profile/"
-    success_message = 'Post was successfully Updated!'
 
     def get_object(self, *args, **kwargs):
         post = super(EditPost, self).get_object(*args, **kwargs)
-        # Checks if logged in user is same as the posts author
-        # if not, renders a 405 response
         if not post.author == self.request.user:
-            return render(request, "error_pages/405.html", status=405)
+            raise Http404
         return post
 
     def form_valid(self, form):
         # This method is called when valid form data has been POSTed.
         # It should return an HttpResponse.
-        print(self.POST)
         return super().form_valid(form)
