5e054ed21d0a3cf0ae7f37bc55262062feded38b
Enquil <jim.e.olesen@gmail.com>
2023-06-29 17:24:10 +0000

    removed add_like and remove_like from test_post_detail_view, this should be a new view test

--- post_detail/test_views.py
+++ post_detail/test_views.py
@@ -1,4 +1,4 @@
-from django.test import TestCase
+from django.test import RequestFactory, TestCase
 from newssite.models import Post, Comment, Category
 from django.contrib.auth.models import User
 from django.shortcuts import (render, get_object_or_404,
@@ -10,77 +10,45 @@ import requests
 
 class TestPostDetailView(TestCase):
 
-    # Test templates when getting the PostDetail view
-    def test_get_post_detail_templates(self):
+    def setUp(self):
 
         category_model = Category.objects.create(
             friendly_name='General',
-            name='general'
         )
-        user = User.objects.create(
-            username='alan',
-            is_superuser=True,
-            password='enigma'
-        )
-        post = Post.objects.create(
-            title='how to crack codes',
-            author=user,
-            content="so easy",
-            category=category_model,
-            slug='how-to-crack-codes'
-        )
-        post.likes.set(('1'))
-        response = self.client.get(reverse('post_detail', args=(post.slug,)))
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'post_detail/post_detail.html')
 
-    def test_add_like(self):
-        category_model = Category.objects.create()
         user = User.objects.create(
             username='alan',
             is_superuser=True,
             password='enigma'
         )
-        post = Post.objects.create(
-            title='how to crack codes',
-            author=user,
-            content="so easy",
-            category=category_model,
-            slug='how-to-crack-codes'
-        )
-        post.likes.set((''))
-        # Add a like test
-        self.assertTrue(post.likes.count() == 0)
-        post.likes.add(user)
-        self.assertTrue(post.likes.count() == 1)
-        # Template test
-        response = self.client.get(reverse('post_detail', args=(post.slug,)))
-        self.assertTrue(post.slug == 'how-to-crack-codes')
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'post_detail/post_detail.html')
 
-    def test_remove_likes(self):
-        category_model = Category.objects.create()
-        # Creates User object to be used as post author
-        user = User.objects.create(
-            username='alan',
-            is_superuser=True,
-            password='enigma'
+        user2 = User.objects.create(
+            username='ada',
+            is_superuser=False,
+            password='difference'
         )
-        # Creates Post object
+
         post = Post.objects.create(
             title='how to crack codes',
             author=user,
             content="so easy",
             category=category_model,
-            slug='how-to-crack-codes'
         )
+
+        post2 = Post.objects.create(
+            title='Calculating machines',
+            author=user2,
+            content="sometimes, 1 is 0",
+            category=category_model,
+        )
+
+    # Test templates when getting the PostDetail view
+    def test_get_post_detail_template(self):
+
+        category_model = Category.objects.get(id=1)
+        user = User.objects.get(id=1)
+        post = Post.objects.get(id=1)
         post.likes.set(('1'))
-        # Remove a like test
-        self.assertFalse(post.likes.count() == 0)
-        post.likes.remove('1')
-        self.assertTrue(post.likes.count() == 0)
-        # Template test
         response = self.client.get(reverse('post_detail', args=(post.slug,)))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'post_detail/post_detail.html')
